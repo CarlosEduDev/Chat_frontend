77dc12dbbf4826cea69049e27e036da5707a7eff
Carlos Eduardo <carloseduardoevang@gmail.com>
2026-03-28 16:56:14 -0400

    feat: trabalhando na lógica da tela de login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './components/login/login';
+import { Chat } from "./components/chat/chat";
 
 @Component({
   selector: 'app-root'  ,
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Login, Chat],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,10 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+  isLoading = false;
+
+  onsubmit(){
+    this.isLoading = true;
+
+    console.log("testando login com " + this.email + " e senha " + this.password);
+  }
+}
