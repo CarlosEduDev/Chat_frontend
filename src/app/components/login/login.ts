001c988becc2c210c8f0aee0033628dd1c0d5f6f
Carlos Eduardo <carloseduardoevang@gmail.com>
2026-03-28 17:35:02 -0400

    feat: validação dos campos de login básica concluída

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -14,9 +14,38 @@ export class Login {
   password = '';
   isLoading = false;
 
-  onsubmit(){
-    this.isLoading = true;
-
-    console.log("testando login com " + this.email + " e senha " + this.password);
+  validateEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   }
+
+  validateFields(email: string, password: string): boolean{
+    if(!email || !password){
+      console.log("Os campos Email e Senha não podem estar vazios.");
+      return false
+    }else if(!this.validateEmail(email)){
+      console.log("Email inválido.");
+      return false;
+    }
+    else if(password.length < 6){
+      console.log("A senha deve conter pelo menos 6 caracteres.");
+      return false;
+    }
+
+    console.log("Login bem-sucedido!");
+    return true;
+  }
+
+  onsubmit(): boolean{
+    this.isLoading = true;
+    // Ao clicar no botão de login, a função onsubmit é chamada. Ela define isLoading como true para indicar que o processo de login está em andamento. Em seguida, ela chama a função validateFields, passando os valores de email e senha para validação. O resultado da validação é retornado, indicando se o login foi bem-sucedido ou não.
+
+    // DEPOIS DE CLICAR NO BOTÃO PRECISO DE ALGUMA FORMA VOLTAR O VALOR DE isLoading PARA FALSE, POIS SE FICAR TRUE ELE VAI FICAR MOSTRANDO O LOADING INDEFINIDAMENTE, ENTÃO PRECISO DE UM TEMPO PARA SIMULAR O PROCESSO DE LOGIN E DEPOIS VOLTAR O VALOR PARA FALSE, ASSIM O LOADING VAI DESAPARECER APÓS ALGUNS SEGUNDOS.
+
+    // APÓS CLICAR NO BOTÃO DE LOGIN, O USER VAI PARA OUTRA ROTA
+
+    return this.validateFields(this.email, this.password);
+  }
+
+  
 }
