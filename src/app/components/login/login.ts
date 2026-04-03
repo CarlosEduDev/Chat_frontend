8a78451eee9c537f98b558393a89825693cdd198
Carlos <carloseduardoevang@gmail.com>
2026-04-03 17:52:10 -0400

    feat: infraestrutura de autenticação stateless no lado do cliente

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +10,22 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnInit{
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('user_token');
+
+    if(token){
+      this.router.navigate(['/chat']);
+    }
+  }
+
   email = '';
   password = '';
   isLoading = false;
   showPassword = false;
+  rememberMe = false;
 
   validateEmail(email: string): boolean {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -41,15 +53,19 @@ export class Login {
     this.showPassword = !this.showPassword;
   }
 
-  onsubmit(): boolean{
+  onsubmit(){
     this.isLoading = true;
-    // Ao clicar no botão de login, a função onsubmit é chamada. Ela define isLoading como true para indicar que o processo de login está em andamento. Em seguida, ela chama a função validateFields, passando os valores de email e senha para validação. O resultado da validação é retornado, indicando se o login foi bem-sucedido ou não.
+    
+    if(this.email === "admin@teste.com" && this.password === "admin123"){
+      localStorage.setItem('user_token', 'token teste gerado no seerver');
+      console.log("Login bem-sucedido!");
+    }
 
-    // DEPOIS DE CLICAR NO BOTÃO PRECISO DE ALGUMA FORMA VOLTAR O VALOR DE isLoading PARA FALSE, POIS SE FICAR TRUE ELE VAI FICAR MOSTRANDO O LOADING INDEFINIDAMENTE, ENTÃO PRECISO DE UM TEMPO PARA SIMULAR O PROCESSO DE LOGIN E DEPOIS VOLTAR O VALOR PARA FALSE, ASSIM O LOADING VAI DESAPARECER APÓS ALGUNS SEGUNDOS.
+    this.router.navigate(['/chat']);
 
-    // APÓS CLICAR NO BOTÃO DE LOGIN, O USER VAI PARA OUTRA ROTA
 
-    return this.validateFields(this.email, this.password);
+
+    // return this.validateFields(this.email, this.password);
   }
 
   
